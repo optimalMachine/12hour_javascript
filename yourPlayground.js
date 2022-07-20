0c3adf1ba463082acc270bbeaad4ecb03ce2e42e
Jiuk Choi <58076159+optimalMachine@users.noreply.github.com>
2022-07-20 01:00:25 +0000

    finish es6 function

--- yourPlayground.js
+++ yourPlayground.js
@@ -1,12 +1,31 @@
 //Tiny Weather App
-let weather = prompt('how is the weather today?')
+// let weather = prompt('how is the weather today?')
 
-if (weather == 'rain'){
-  console.log(`it's rainy day today.`)
-}else {
-  console.log('Wear your sunglasses.')
+// if (weather == 'rain'){
+//   console.log(`it's rainy day today.`)
+// }else {
+//   console.log('Wear your sunglasses.')
+// }
+
+function twoSum(a,b){
+  return a+b
 }
 
+//Simple Tip calculator
+function tipCalculator (){
+  let foodPrice = Number(prompt('How much is the food?'))
+  let tipPercentage = Number(prompt('tip %?'))
+  let totalTip = foodPrice * (tipPercentage / 100)
+  let totalMoney = twoSum(foodPrice,totalTip)
+  console.log(`the tip is $${totalTip}`)
+  console.log(`the total is $${totalMoney}`)
+  return totalMoney
+}
+
+//tipCalculator()
+
+//ES6 =>
+const sumArrow = (a,b) => a+b
 
 
 
