360625a141d2fda3ccabce08357a79b6f48f4c42
Jiuk Choi <58076159+optimalMachine@users.noreply.github.com>
2022-07-22 04:54:08 +0000

    finishes for loop word & letter exercise

--- yourPlayground.js
+++ yourPlayground.js
@@ -7,16 +7,16 @@
 //   console.log('Wear your sunglasses.')
 // }
 
-function twoSum(a,b){
-  return a+b
+function twoSum(a, b) {
+  return a + b
 }
 
 //Simple Tip calculator
-function tipCalculator (){
+function tipCalculator() {
   let foodPrice = Number(prompt('How much is the food?'))
   let tipPercentage = Number(prompt('tip %?'))
   let totalTip = foodPrice * (tipPercentage / 100)
-  let totalMoney = twoSum(foodPrice,totalTip)
+  let totalMoney = twoSum(foodPrice, totalTip)
   console.log(`the tip is $${totalTip}`)
   console.log(`the total is $${totalMoney}`)
   return totalMoney
@@ -25,9 +25,9 @@ function tipCalculator (){
 //tipCalculator()
 
 //ES6 =>
-const sumArrow = (a,b) => a+b
+const sumArrow = (a, b) => a + b
 
-let groceries = ['banana','apple','orange','pear']
+let groceries = ['banana', 'apple', 'orange', 'pear']
 groceries.push('cookie')
 // console.log(groceries)
 // console.log(groceries.slice(0,2))
@@ -36,9 +36,9 @@ groceries.push('cookie')
 
 
 //Object
-const person={
-  name:'Leonardo',
-  shirt:'white'
+const person = {
+  name: 'Leonardo',
+  shirt: 'white'
 }
 
 // console.log(person.name)
@@ -52,8 +52,8 @@ const person={
 
 
 const person_02 = {
-  name:'Qazi',
-  shirt:'black'
+  name: 'Qazi',
+  shirt: 'black'
 }
 
 const introducer = (name, shirt) => {
@@ -73,7 +73,7 @@ const introducer = (name, shirt) => {
 
 
 //loop
-let fruits = ['banana','apple','orange','pear','banana','apple','orange','pear','banana','apple','orange','pear','banana','apple','orange','pear']
+let fruits = ['banana', 'apple', 'orange', 'pear', 'banana', 'apple', 'orange', 'pear', 'banana', 'apple', 'orange', 'pear', 'banana', 'apple', 'orange', 'pear']
 
 // for (let i=0; i<fruits.length; i++){
 //       console.log(fruits[i])
@@ -105,6 +105,88 @@ let fruits = ['banana','apple','orange','pear','banana','apple','orange','pear',
 //   }
 //   return result
 // }
-// console.log(doubleNumber(numbers))
+// console.l og(doubleNumber(numbers))
 
+//The way to count the number of letters.
+const howManyLetters = () => {
+  const phrase = '씨발놈아';
+  let result = 0;
+  for (let letter in phrase) {
+    //console.log(Number(letter) + 1)
+    result = Number(letter) + 1
+  }
+  return { result }
+}
 
+// console.log(howManyLetters())
+
+const sumArray = () => {
+  let a = []
+  let result = 0
+  for (let i = 0; i <= 10; i++) {
+    a.push(Number(i))
+  }
+  for (const j in a) {
+    result += Number(j);
+  }
+  return { result }
+}
+
+const sumArray_02 = () => {
+  let a = []
+  let result = 0
+  for (let i = 0; i <= 10; i++) {
+    a.push(Number(i))
+  }
+  for (const j in a) {
+    result += Number(j);
+  }
+  return a
+}
+
+const maxNumber = (a) => {
+  let result = a[0];
+  for (const number of a) {
+    if (number > result) {
+      result = number
+    }
+  }
+  return { result };
+}
+
+// console.log(sumArray())
+// console.log(maxNumber(sumArray_02()))
+
+const letterFrequency = (phrase) => {
+  let result = {}
+  for (const letter of phrase) {
+    if (letter in result) {
+      result[letter] = result[letter]++;
+    }else{
+      result[letter] = 1;
+    }
+  }
+  return result
+}
+
+//console.log(letterFrequency('씨발놈아'))
+
+const wordFrequency = (phrase) =>{
+  let result = {}
+  let replaceTexts = phrase.replaceAll('.' , '')
+  let replaceTexts2 = replaceTexts.replaceAll(',' ,'')
+  let texts = replaceTexts2.split(" ")
+  console.log(texts)
+  for (const word of texts){
+    console.log(word)
+    if (word in result){
+      result[word] += 1
+    }else{
+      result[word] = 1
+    }
+  }
+
+  return result
+}
+
+//console.log(wordFrequency('lol. what, lol'))
